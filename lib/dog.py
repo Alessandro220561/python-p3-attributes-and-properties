0a300c06363b98571de4e4e8f0a22eb93eb5a1bc
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2023-09-11 13:57:07 -0400

    lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,34 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name="Lucas", breed="Corgi"):
+        if self.is_valid_name(name):
+            self.name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if self.is_valid_breed(breed):
+            self.breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    def is_valid_name(self, name):
+        return isinstance(name, str) and 1 <= len(name) <= 25
+
+    def is_valid_breed(self, breed):
+        return breed in APPROVED_BREEDS
+
+
+# class Dog:
+#     def __init__(self, name="Lucas", breed="Corgi"):
+#         if isinstance(name, str) and 1 <= len(name) <= 25:
+#             self.name = name
+#         else:
+#             print("Name must be string between 1 and 25 characters.")
+
+#         if breed in APPROVED_BREEDS:
+#             self.breed = breed
+#         else:
+#             print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,36 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
+# class Person:
+#     def __init__(self, name="Alessandro", job="Legal"):
+#         if self.is_valid_name(name):
+#             self.name = name.title()
+#         else:
+#             print("Name must be string between 1 and 25 characters.")
+
+#         if self.is_valid_job(job):
+#             self.job = job
+#         else:
+#             print("Job must be in list of approved jobs.")
+
+#     def is_valid_name(self, name):
+#         valid_name = str(isinstance(name, str) and 1 >=
+#                          len(name) <= 25)
+#         return valid_name
+
+#     def is_valid_job(self, job):
+#         return job in APPROVED_JOBS
+
+
 class Person:
-    pass
+    def __init__(self, name="Jon", job="Legal"):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self.name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if job in APPROVED_JOBS:
+            self.job = job
+        else:
+            print("Job must be in list of approved jobs.")
